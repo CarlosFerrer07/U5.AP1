4812002ea56680d2c9b755d08234a8d2895afac8
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-01-23 15:49:46 +0100

    ejercicio 2 finalizado

---
+++ ej2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#pedimos eleccion al usuario
+
+read -p "Introduce un numero mayor que 0: " eleccion
+
+#comprobamos que el valor sea mayor que 0
+
+while [ $eleccion -le 0 ]; do 
+    echo "el numero introducido no es mayor que 0, vuelve a introducirlo"
+    read -p "Introduce un numero mayor que 0 " eleccion
+done
+
+#nos guardamos la variable resto
+
+let resto=eleccion%2
+
+#condicion para saber si es par o impar
+if [ $resto -eq 0 ]; then
+    echo "El numero $eleccion es par"
+else
+   echo "El numero $eleccion es impar" 
+fi
