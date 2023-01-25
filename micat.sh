1c4c1d75bfb5b6503d98701ca65b121ff8b0deee
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-01-25 17:50:14 +0100

    Falta el punto d de micat

---
+++ micat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "ruta fichero "$1
+echo "Introduce -r para invertir" $2
+
+echo "-----------------------"
+
+echo "-----------------------"
+
+if test -f $1 ; then
+    cat $1
+else 
+    echo "No existe el fichero"
+fi
+
+#Condicion solo en caso de k se añada un segundo parametro
+
+if [ "$2" ]; then #BASICAMENTE "$2" comprueba si existe
+    if [ $2 = -r ]; then
+        echo "----------"
+        cat $1
+    fi
+fi
+
+
