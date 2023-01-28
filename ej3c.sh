a5a32e3ba6c39a82e6d69f612170908c64fad01b
Carlos Ferrer <cafema01@floridauniversitaria.es>
2023-01-28 15:44:54 +0100

    Finalizamos el ejercicio 3 del bloque 3

---
+++ ej3c.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Introduzca una palabra: "
+read palabra
+
+cont=0
+#Inicializamos el array
+words=()
+
+while [ $palabra != "fin" ]; do
+    cont=$(($cont + 1 ))
+    words+=("$palabra") # añadimos las palabras al array
+    echo "Introduzca una palabra: "
+    read palabra
+done;
+
+echo "Se han introducido $cont palabras"
+
+echo "Palabras ingresadas: "
+sleep 1
+echo "${words[@]}" | tr ' ' '\n' | sort # el comando tr es para el salto de linia y el sort ordena las palabras alfabéticamente
